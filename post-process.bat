c46facbe0b88f1fca127d08b070178fcbe8601c8
sravanthigujjari <42617149+sravanthigujjari@users.noreply.github.com>
2018-08-27 14:54:21 -0400

    Update post-process.bat

--- post-process.bat
+++ post-process.bat
@@ -1,15 +1,3 @@
-Pre Processing commands:
-
-- get file from server (connect to server, get the file from server and put it in required location)
-and load to target table 
-
-Post Processing commands::
-
-Run SP
-Rename
-archive
-
-REM Batch file we wrote----
 set batchfile=%~n0
 call D:\app\infa\batch\initialize.bat
 
@@ -18,11 +6,7 @@ set REMOTE_FILE_LOCATION=/PartnerData/SingleCare
 set REMOTE_FILE_NAME=sc_salesrepsegment.csv
 set RENAMED_FILE=sc_salesrepsegment_%DATE_YYYY%%DATE_MM%%DATE_DD%.csv
 
-echo cd %REMOTE_FILE_LOCATION% > %PSFTP_FILE%
-echo get %REMOTE_FILE_NAME% >> %PSFTP_FILE%
 cd D:\data\B2B2C_Actions
-
-psftp ftp.scriptrelief.com -l informaticauser -pw MS#@8AKqjSD2$t7z -P 22 -b %PSFTP_FILE% -be >> %loginfo%
 
 if exist %REMOTE_FILE_NAME% (
     
@@ -44,12 +28,12 @@ REM sqlcmd.exe -S Simpleplace.SingleCare.com -U mercatus3 -P S1ngl3MeNow -d Sing
 )
 
 :got_file
-echo got Files %DATE% %TIME% > %logsuccess%
-echo got Files %DATE% %TIME% 
+echo got Files %DATE% %TIME% >> %logsuccess%
+echo got Files %DATE% %TIME% >> %logsuccess%
 goto done
 
 :file_not_found
-echo Could Not Find Files. %DATE% %TIME% > %logerror%
+echo Could Not Find Files. %DATE% %TIME% >> %logerror%
 echo Could not find files.
 
 :done
