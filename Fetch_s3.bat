f029b6e8066c363f415b2c5ac9f01f53ff58c7c6
sravanthigujjari <42617149+sravanthigujjari@users.noreply.github.com>
2018-11-27 13:57:42 -0500

    Update Fetch_s3.bat

--- Fetch_s3.bat
+++ Fetch_s3.bat
@@ -17,9 +17,9 @@ if exist %PSFTP_FILE% del %PSFTP_FILE%
 if exist %processfile% (
   ren %processfile% %RENAMED_FILE%
   echo Sending files to S3 bucket. >> %loginfo%
-  aws s3 cp %RENAMED_FILE% %S3_bucket_filepath% --profile Fetch
-  7z.exe a "D:\data\outbound\FetchExport\SC_Fetch_Export_Out_%DATE_YYYY%%DATE_MM%%DATE_DD%.zip" "D:\data\outbound\FetchExport\%RENAMED_FILE%"
-  febootimail -config "D:\app\infa\batch\febooti_config_sc_Fetch_test.txt"
+  aws s3 cp %RENAMED_FILE% %S3_bucket_filepath% --profile Fetch >> %loginfo%
+  7z.exe a "D:\data\outbound\FetchExport\SC_Fetch_Export_Out_%DATE_YYYY%%DATE_MM%%DATE_DD%.zip" "D:\data\outbound\FetchExport\%RENAMED_FILE%" >> %loginfo%
+  febootimail -config "D:\app\infa\batch\febooti_config_sc_Fetch_test.txt" >> %loginfo%
   move /Y D:\data\outbound\FetchExport\SC_Fetch_Export_Out_*.zip %archive% >> %loginfo%
   goto got_file
 ) else (
