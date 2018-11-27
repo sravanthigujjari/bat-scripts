a3b9e5af019a4dd6b09368ab65a76d4479fc0cea
sravanthigujjari <42617149+sravanthigujjari@users.noreply.github.com>
2018-11-27 14:37:13 -0500

    Update Fetch_s3.bat

--- Fetch_s3.bat
+++ Fetch_s3.bat
@@ -11,16 +11,21 @@ set PSFTP_FILE=D:\data\FTPScripts\PSFTP_sc_fetch_test.txt
 set S3_bucket_filepath=s3://rxsense-partner-data-qa/fetch/temp_archive/
 set path=%path%;c:\Program Files\7-Zip
 
-
-if exist %PSFTP_FILE% del %PSFTP_FILE%
+echo "1" >> %loginfo%
 
 if exist %processfile% (
   ren %processfile% %RENAMED_FILE%
+  echo "2" >> %loginfo%
   echo Sending files to S3 bucket. >> %loginfo%
+  echo "3" >> %loginfo%
   aws s3 cp %RENAMED_FILE% %S3_bucket_filepath% --profile Fetch >> %loginfo%
+  echo "4" >> %loginfo%
   7z.exe a "D:\data\outbound\FetchExport\SC_Fetch_Export_Out_%DATE_YYYY%%DATE_MM%%DATE_DD%.zip" "D:\data\outbound\FetchExport\%RENAMED_FILE%" >> %loginfo%
+  echo "5" >> %loginfo%
   febootimail -config "D:\app\infa\batch\febooti_config_sc_Fetch_test.txt" >> %loginfo%
+  echo "6" >> %loginfo%
   move /Y D:\data\outbound\FetchExport\SC_Fetch_Export_Out_*.zip %archive% >> %loginfo%
+  echo "7" >> %loginfo%
   goto got_file
 ) else (
     goto file_not_found
